940ba90ce25646c08f4a6d33b2ce8bc546bb5260
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-01-03 01:03:07 +0500

    auth guard not working

--- src/authentication/controllers/authentication.controller.ts
+++ src/authentication/controllers/authentication.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, HttpCode, Param, Post, Query } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 import { logInDto } from "../dtos/log-in.dto";
 import { AuthenticationService } from "../services/authentication.service";
+import { skipAuth } from "src/utils/decorators/skip-auth.decorator";
 
 @Controller("v1/auth")
 @ApiTags("Authentication")
@@ -10,6 +11,7 @@ export class AuthenticationController {
         private readonly authenticationService: AuthenticationService
     ) {}
 
+    @skipAuth()
     @Post("/login")
     @HttpCode(200)
     logIn(@Body() logInDto: logInDto) {
--- src/authentication/services/authentication.service.ts
+++ src/authentication/services/authentication.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from "@nestjs/common";
 import { LoginService } from "./login.service";
 import { PlainPassword } from "src/utils/value-objects/password.vo";
 import { JWTService } from "src/utils/commonservices/jwt.service";
+import { TokenDto } from "src/utils/commonDtos/token.dto";
+import id from "zod/v4/locales/id.js";
+import { boolean } from "zod";
 
 @Injectable()
 export class AuthenticationService {
@@ -10,12 +13,29 @@ export class AuthenticationService {
         private readonly jwtService: JWTService
     ) {}
 
-    async verifyUser(email: string, pass: PlainPassword): Promise<any> {
+    async verifyUser(email: string, pass: PlainPassword): Promise<TokenDto | null> {
             const user = await this.loginService.logIn(email, pass);
             const accessToken = this.jwtService.generateAccesToken(user.id, email);
             const refreshToken = this.jwtService.generateRefreshToken(user.id, email);
             return{
-                data: user,
+                id: user.id,
+                firstName: user.firstName,
+                middleName: user.middleName,
+                lastName: user.lastName,
+                email: user.email,
+                emailVerifiedAt: user.emailVerifiedAt,
+                phoneNumber1: user.phoneNumber1,
+                phoneNumber2: user.phoneNumber2,
+                failedAttempts: user.failedAttempts,
+                isPasswordReset: user.isPasswordReset,
+                isNotificationEnabled: user.isNotificationEnabled,
+                lastFailedAttempt: user.lastFailedAttempt,
+                lockedUntil: user.lockedUntil,
+                status: user.status,
+                isPatient: user.isPatient,
+                isAdmin: user.isAdmin,
+                isPrincipalInvestigator: user.isPrincipalInvestigator,
+                isSuperUser: user.isSuperUser,
                 accessToken,
                 refreshToken
             };
