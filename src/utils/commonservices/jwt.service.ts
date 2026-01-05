f7ab430cd86dc7462eac680a8c45cba7061eac42
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-01-05 22:22:50 +0500

    token generation achieved

--- src/utils/commonservices/jwt.service.ts
+++ src/utils/commonservices/jwt.service.ts
@@ -1,6 +1,6 @@
 import { ConfigService } from "@nestjs/config";
 import { JwtService } from "@nestjs/jwt";
-import { Injectable } from "@nestjs/common";
+import { Injectable, Req, UnauthorizedException } from "@nestjs/common";
 
 export class JwtPayload {
     sub: string;
@@ -45,4 +45,32 @@ export class JWTService {
             expiresIn: this.configService.get<number>('JWT_REFRESH_EXPIRES_IN'),
         })
     }
-}
+
+
+    async refresh(refreshToken: string) {
+        try{
+            const payload = await this.jwtService.verifyAsync(refreshToken,
+            {
+                secret: this.configService.get<string>('JWT_REFRESH_SECRET')
+            }
+            )
+
+            if (payload.type !== 'refresh') {
+                throw new UnauthorizedException("Invalid Token Type");
+            }
+            
+            const newAccessToken = this.generateAccesToken(
+                payload.sub,
+                payload.email
+            )
+            console.log(newAccessToken);
+
+            return {
+                newAccessToken
+            };
+        }catch (error) {
+            console.error('Refresh token error:', error.message);
+            throw new UnauthorizedException('Invalid or expired refresh token');
+        }
+    }
+}   
