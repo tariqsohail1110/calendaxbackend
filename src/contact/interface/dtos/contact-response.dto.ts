b0035d42cea34f236e423b36843a3f984d3cfcc4
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-01-15 02:06:50 +0500

    contact module removed

--- src/contact/interface/dtos/contact-response.dto.ts
+++
@@ -1,9 +0,0 @@
-export class ContactResponseDto{
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone_number: string;
-    note: string;
-    reply: string;
-}
